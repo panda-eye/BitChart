38b8568e143cca7bad49e6b1f31c5af342f9eb92
kk05 <panda25051@gmail.com>
2018-01-23 20:46:46 +0200

    Code changes

--- app/src/main/java/com/bitchartdev/bitchart/Tasks/BitfinesTask.kt
+++ app/src/main/java/com/bitchartdev/bitchart/Tasks/BitfinesTask.kt
@@ -3,26 +3,46 @@ package com.bitchartdev.bitchart.Tasks
 import android.os.AsyncTask
 import com.bitchartdev.bitchart.BitChartActivity
 import com.bitchartdev.bitchart.R
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.lang.ref.WeakReference
 
 /**
  * Created by bitchartdev on 23.01.2018.
  */
 
-class BitfinesTask : AsyncTask<WeakReference<BitChartActivity>, Unit, WeakReference<BitChartActivity>>() {
-    override fun doInBackground(vararg p: WeakReference<BitChartActivity>): WeakReference<BitChartActivity> {
-        val activity = p[0].get() ?: return p[0]
-        val response = TaskHelper.getResponse(activity.getString(R.string.bitfinex_link))
-        if (response != null) {
-            // TODO: Parse response
-        } else {
-            // TODO: Catch error
+class BitfinesTask : AsyncTask<WeakReference<BitChartActivity>, Unit, Pair<WeakReference<BitChartActivity>, LinkedHashMap<String, String>>>() {
+    override fun doInBackground(vararg p: WeakReference<BitChartActivity>): Pair<WeakReference<BitChartActivity>, LinkedHashMap<String, String>> {
+        val activity = p[0].get() ?: return Pair(p[0], LinkedHashMap())
+        val response = try {
+            TaskHelper.getResponse(activity.getString(R.string.bitfinex_link))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
         }
-        return p[0]
+        val parsed = if (response != null) {
+            try {
+                TaskHelper.mapper.readValue<LinkedHashMap<String, String>>(response)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                LinkedHashMap<String, String>()
+            }
+        } else {
+            TaskHelper.showTaskSnackbar(activity.forSnackbarView, R.string.update_failed)
+            LinkedHashMap()
+        }
+        return Pair(p[0], parsed)
     }
-    override fun onPostExecute(result: WeakReference<BitChartActivity>) {
-        val activity = result.get() ?: return
-        // TODO: Update data
-        super.onPostExecute(result)
+    override fun onPostExecute(pair: Pair<WeakReference<BitChartActivity>, LinkedHashMap<String, String>>) {
+        super.onPostExecute(pair)
+        val parsed = pair.second
+        if (parsed.isNotEmpty()) {
+            val activity = pair.first.get() ?: return
+            try {
+                val market = TaskHelper.Companion.Market()
+                market.min24h = parsed["low"] ?: ""
+                market.max24h = parsed["high"] ?: ""
+                activity.readBitfines(market)
+            } catch (e: Exception) {e.printStackTrace()}
+        }
     }
 }
--- app/src/main/java/com/bitchartdev/bitchart/Tasks/YoBitTask.kt
+++ app/src/main/java/com/bitchartdev/bitchart/Tasks/YoBitTask.kt
@@ -3,25 +3,47 @@ package com.bitchartdev.bitchart.Tasks
 import android.os.AsyncTask
 import com.bitchartdev.bitchart.BitChartActivity
 import com.bitchartdev.bitchart.R
+import com.fasterxml.jackson.module.kotlin.readValue
 import java.lang.ref.WeakReference
 
 /**
  * Created by bitchartdev on 23.01.2018.
  */
-class YoBitTask: AsyncTask<WeakReference<BitChartActivity>, Unit, WeakReference<BitChartActivity>>() {
-    override fun doInBackground(vararg p: WeakReference<BitChartActivity>): WeakReference<BitChartActivity> {
-        val activity = p[0].get() ?: return p[0]
-        val response = TaskHelper.getResponse(activity.getString(R.string.yobit_link))
-        if (response != null) {
-            // TODO: Parse response
-        } else {
-            // TODO: Catch error
+class YoBitTask: AsyncTask<WeakReference<BitChartActivity>, Unit, Pair<WeakReference<BitChartActivity>, LinkedHashMap<String, Any>>>() {
+    override fun doInBackground(vararg p: WeakReference<BitChartActivity>): Pair<WeakReference<BitChartActivity>, LinkedHashMap<String, Any>> {
+        val activity = p[0].get() ?: return Pair(p[0], LinkedHashMap())
+        val response = try {
+            TaskHelper.getResponse(activity.getString(R.string.yobit_link))
+        } catch (e: Exception) {
+            e.printStackTrace()
+            null
         }
-        return p[0]
+        val parsed = if (response != null) {
+            try {
+                TaskHelper.mapper.readValue<LinkedHashMap<String, Any>>(response)
+            } catch (e: Exception) {
+                e.printStackTrace()
+                LinkedHashMap<String, Any>()
+            }
+        } else {
+            TaskHelper.showTaskSnackbar(activity.forSnackbarView, R.string.update_failed)
+            LinkedHashMap()
+        }
+        return Pair(p[0], parsed)
     }
-    override fun onPostExecute(result: WeakReference<BitChartActivity>) {
-        val activity = result.get() ?: return
-
-        super.onPostExecute(result)
+    @Suppress("UNCHECKED_CAST")
+    override fun onPostExecute(pair: Pair<WeakReference<BitChartActivity>, LinkedHashMap<String, Any>>) {
+        super.onPostExecute(pair)
+        val parsed = pair.second
+        if (parsed.isNotEmpty()) {
+            val activity = pair.first.get() ?: return
+            try {
+                val data = parsed["btc_usd"] as LinkedHashMap<String, Double>
+                val market = TaskHelper.Companion.Market()
+                market.min24h = data["low"].toString()
+                market.max24h = data["high"].toString()
+                activity.readYoBit(market)
+            } catch (e: Exception) {e.printStackTrace()}
+        }
     }
 }
